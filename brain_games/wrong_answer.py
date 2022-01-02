770e37bc2d2486a92e240918921a783712e4b6f7
Vladislav Isupov <visupov@MacBook-Air-Vladislav.local>
2022-01-02 17:55:40 +0300

    fix linter errors

--- brain_games/wrong_answer.py
+++ brain_games/wrong_answer.py
@@ -2,5 +2,6 @@ from typing import Any, Text
 
 
 def wrong_answer(name: Text, correct_answer: Any, wrong_answer: Any) -> None:
-    print(f"'{wrong_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+    print(f"'{wrong_answer}' is wrong answer ;(. ' \
+    'Correct answer was '{correct_answer}'.")
     print(f"Let's try again, {name}!")
