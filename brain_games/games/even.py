2859ea1e1f6bc03f161c6e0efc97c5878314acd7
Vladislav Isupov <visupov@MacBook-Air-Vladislav.local>
2022-01-02 18:04:39 +0300

    fix linter errors

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -7,8 +7,8 @@ from brain_games.congratulations import congrantulations
 
 
 def game() -> None:
-    name = greeting('Answer "yes" if the number is even, '
-    'otherwise answer "no".')
+    name = greeting("Answer \"yes\" if the number is even, " \
+    "otherwise answer \"no\".")
     for _ in range(3):
         number = random.randint(0, 100)
         print(f'Question: {number}')
